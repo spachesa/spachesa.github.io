90eedfedd17f8ec5759d2c120e241243f7e7b993
spachesa <spxnyc@gmail.com>
2017-08-26 16:01:01 -0400

    Update rand.js

--- rand.js
+++ rand.js
@@ -1,4 +1,5 @@
 var pickButton;
+var pickButton2;
 var header;
 var body;
 var choice;
@@ -32,16 +33,18 @@ var colors = [
 ];
 var cur = 0;
 
-
-  console.log('hello rand.js');
+document.addEventListener("DOMContentLoaded", () => {
+    console.log('hello rand.js');
 
   body = document.getElementById('body');
   header = document.getElementById('header');
   pickButton = document.getElementById('pickButton');
+  pickButton2 = document.getElementById('pickButton2');
   choice = document.getElementById('choice');
   aList = document.getElementById('aList');
 
   pickButton.addEventListener('click', pickOnClick );
+  pickButton2.addEventListener('click', clickpick );
   aList.addEventListener('click', aListOnClick );
 
   // build the attendance list
@@ -50,10 +53,27 @@ var cur = 0;
     li.innerText = students[i];
     aList.append(li);
   }
+});
+ 
 
 var pickOnClick = function (event) {
   choice.innerHTML = '&nbsp;'
-  var rand = students[Math.floor(Math.random() * students.length)];
+  var rand = students.splice(Math.floor(Math.random() * students.length),1);
+  var x = window.setInterval(() => {
+    if (colors[cur] === undefined) {
+      window.clearInterval(x);
+      cur = 0;
+      choice.innerText = rand;
+      return;
+    }
+    if (colors[cur]) header.style.color = colors[cur];
+    if (colors[cur-1]) body.style.backgroundColor = colors[cur-1];
+    cur += 1;
+  }, 200);
+}
+
+var clickpick = function (event) {
+  alert("hey!!!");
   var x = window.setInterval(() => {
     if (colors[cur] === undefined) {
       window.clearInterval(x);
@@ -72,3 +92,4 @@ var aListOnClick = function (event) {
     event.target.style.textDecoration = 'line-through';
   }
 }
+
