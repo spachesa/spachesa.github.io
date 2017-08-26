2bc24cebffc2056d69f8e2bc2a580e665e3800b7
spachesa <spxnyc@gmail.com>
2017-08-26 16:05:12 -0400

    Update rand.js

--- rand.js
+++ rand.js
@@ -1,95 +1,34 @@
-var pickButton;
-var pickButton2;
-var header;
-var body;
-var choice;
-var aList;
-
-var students = [
-  "Kevin",
-  "Jonathan",
-  "Angel",
-  "Eric",
-  "Janette",
-  "Desmond",
-  "Ashiya",
-  "Monica",
-  "Susana",
-  "Deborah",
-  "Thomas",
-  "Julissa",
-  "Kyla",
-  "John",
-  "Shonica",
-  "Krystal"
-];
-
-var colors = [
-  'hotpink',
-  'orange',
-  'fuchsia', 0,
-  'lightblue', 0,
-  'black'
-];
-var cur = 0;
-
-document.addEventListener("DOMContentLoaded", () => {
-    console.log('hello rand.js');
-
-  body = document.getElementById('body');
-  header = document.getElementById('header');
-  pickButton = document.getElementById('pickButton');
-  pickButton2 = document.getElementById('pickButton2');
-  choice = document.getElementById('choice');
-  aList = document.getElementById('aList');
-
-  pickButton.addEventListener('click', pickOnClick );
-  pickButton2.addEventListener('click', clickpick );
-  aList.addEventListener('click', aListOnClick );
-
-  // build the attendance list
-  for (let i=0; i<students.length; i++) {
-    let li = document.createElement('li');
-    li.innerText = students[i];
-    aList.append(li);
-  }
-});
- 
-
-var pickOnClick = function (event) {
-  choice.innerHTML = '&nbsp;'
-  var rand = students.splice(Math.floor(Math.random() * students.length),1);
-  var x = window.setInterval(() => {
-    if (colors[cur] === undefined) {
-      window.clearInterval(x);
-      cur = 0;
-      choice.innerText = rand;
-      return;
-    }
-    if (colors[cur]) header.style.color = colors[cur];
-    if (colors[cur-1]) body.style.backgroundColor = colors[cur-1];
-    cur += 1;
-  }, 200);
+html {
+  margin: 0;
+  padding: 0;
 }
 
-var clickpick = function (event) {
-  alert("hey!!!");
-  var x = window.setInterval(() => {
-    if (colors[cur] === undefined) {
-      window.clearInterval(x);
-      cur = 0;
-      choice.innerText = rand;
-      return;
-    }
-    if (colors[cur]) header.style.color = colors[cur];
-    if (colors[cur-1]) body.style.backgroundColor = colors[cur-1];
-    cur += 1;
-  }, 200);
+body {
+  font-size: 24px;
+  width: 25em;
+  margin: 0 auto;
+  padding: 0.5em 0;
+
+  height: 100vh;
+
+  font-family: monospace;
+  background-color: darkblue;
+  text-align: center;
+  
 }
 
-var aListOnClick = function (event) {
-  if (event.target.tagName === 'LI') {
-    event.target.style.textDecoration = 'line-through';
-  }
+button {
+  font-size: 20px;
+  font-family: Courier, Courier, sans-serif;
+}
+
+ul {
+  list-style-type: none;
+  background-color: lightblue;
+  text-decoration-color: darkblue;
+}
+
+img {
+  max-width: 60%;
 }
 
